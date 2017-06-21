2dafa0fff9a6dca722a41a023261b357d65001ef
Thomas Stensitzki <thomas@mcsmemail.de>
2017-06-21 14:13:28 +0200

    Filter attribute changed from LastAccessTime to LastWriteTime

--- Copy-ScriptsToRepository.ps1
+++ Copy-ScriptsToRepository.ps1
@@ -7,20 +7,16 @@
   THIS CODE IS MADE AVAILABLE AS IS, WITHOUT WARRANTY OF ANY KIND. THE ENTIRE 
   RISK OF THE USE OR THE RESULTS FROM THE USE OF THIS CODE REMAINS WITH THE USER.
 	
-  Version 1.0, 2017-03-01
+  Version 1.1, 2017-06-21
 
   Please send ideas, comments and suggestions to support@granikos.eu 
  
   .LINK  
-  http://scripts.granikos.eu 
+  More information can be found at http://www.granikos.eu/en/scripts 
 	
   .DESCRIPTION
 	
-  This script copies files (.ps1, .cmd, .xml) to a new target while persisting the directory structure.
-
-  *.log files are excluded from being copied to the target directory.
-
-  Only new files and files changed during the last 180 days are copied. 
+  This script copies files (.ps1, .cmd, .xml) to a new target while persisting the directory structure
 
   .NOTES 
   Requirements 
@@ -28,10 +24,11 @@
 
   Revision History 
   -------------------------------------------------------------------------------- 
-  1.0     Initial community release 
+  1.0 Initial community release
+  1.1 Filter attribute changed from LastAccessTime to LastWriteTime 
 	
   .PARAMETER Source
-  Source directory containing all files that need to be copied
+  Source directory contaiing all files thatneed to be copied
 
   .PARAMETER Destination
   Destination directory
@@ -49,15 +46,10 @@
 
 #>
 
+[CmdletBinding()]
 Param(
-  [CmdletBinding]
-  # Set default value to your preferred source location
-  [parameter()]
-  [string]$Source = "$env:HOMEDRIVE\Scripts",   
-  # Set default value to your preferred targetlocation
-  [parameter()]
-  [string]$Destination = '\\MYFILESERVER\SharedScripts\Scripts\',
-  [parameter()]
+  [string]$Source = 'D:\AutomatedServices\Exchange-Skripte\',  
+  [string]$Destination = '\\bdr.de\daten\Medien\Software\Freigegeben\Lizenzpflichtig\microsoft\exchange\Scripts\',
   [switch]$Overwrite
 )
 
@@ -72,39 +64,39 @@ Write-Output "Copy to  : $($Destination)"
 $since = (Get-Date).AddDays(-180)
 
 # Copy only files having the following file extensions
-$includes = '*.ps1*','*.cmd','*.xml'
+$includes = "*.ps1*","*.cmd","*.xml"
 
 # Do *NOT* copy the followig files
-$excludes = '*.log'
+$excludes = "*.log"
 
 # Fetch files that need to be copie
-$items = Get-ChildItem -Path $Source -Recurse -Include $includes -Exclude $excludes | Where-Object {$_.LastAccessTime -gt $since}
+$items = Get-ChildItem $Source -Recurse -Include $includes -Exclude $excludes | Where-Object {$_.LastWriteTime –gt $since}
 
-Write-Verbose -Message "$($items.Count) files found"
+Write-Verbose "$($items.Count) files found"
 
 foreach ($item in $items) {
-    Write-Verbose -Message "Working on: $($item.FullName)"
+    Write-Verbose "Working on: $($item.FullName)"
     $dir = $item.DirectoryName.Replace($Source,$Destination)
     $target = $item.FullName.Replace($Source,$Destination)
 
     # Create target directory, if not exists
-    if (!(Test-Path -Path ($dir))) { 
-        Write-Verbose -Message "Creating destination folder: $($dir)"
-        mkdir -Path $dir | Out-Null
+    if (!(Test-Path($dir))) { 
+        Write-Verbose "Creating destination folder: $($dir)"
+        mkdir $dir | Out-Null
     }
 
     # Copy files
-    if (!(Test-Path -Path ($target))) {
-        Write-Verbose -Message "Copy: $($item.FullName)"
+    if (!(Test-Path($target))) {
+        Write-Verbose "Copy: $($item.FullName)"
         Copy-Item -Path $item.FullName -Destination $target -Recurse -Force | Out-Null
     }
     else {
         if($Overwrite) {
-            Write-Verbose -Message "Overwrite: $($item.FullName)"
+            Write-Verbose "Overwrite: $($item.FullName)"
             Copy-Item -Path $item.FullName -Destination $target -Recurse -Force | Out-Null
         }
         else {
-            Write-Verbose -Message "Skip: $($item.FullName)"
+            Write-Verbose "Skip: $($item.FullName)"
         }
     }
 }
